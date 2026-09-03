44d1e15eceb9eeedb047f5117da685e64c60ba04
Matt Carr <mhcarr17@gmail.com>
2026-09-03 09:10:53 -0600

    Added directories from lab 1

---
+++ labs/lab_1/the_basics.py
@@ -0,0 +1,33 @@
+# Matt Carr - APPM 4600 Lab 1
+# 3.2 Exercises: The Basics
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Create two Numpy Arrays
+
+x = np.linspace(0, 100, num=50)
+y = np.arange(0, 50)
+
+
+# Print first three entries of x
+print('the first three entries of x are: ', x[:3])
+
+# vector w
+w = 10**(-np.linspace(1,10,10))
+
+# w is a vector with increasing orders of magnitude
+
+x = np.arange(0,len(w))
+
+plt.semilogy(x, w)
+plt.xlabel('X')
+plt.ylabel('W')
+plt.title('X vs W Seminology Plot')
+
+s = 3*w
+
+plt.semilogy(x, s)
+
+plt.show()
+
